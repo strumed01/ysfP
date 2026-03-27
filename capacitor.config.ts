e255af1f28bfd3c024912731cdfcfaa07617df37
strumed01 <mohamedamoud.sri@gmail.com>
2026-03-27 18:38:41 +0100

    Flatten structure for Appflow success

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'credix-frontend',
-  webDir: 'build'
+  webDir: 'build',
 };
 
 export default config;
